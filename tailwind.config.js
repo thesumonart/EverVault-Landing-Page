cd7a79485a76956ffe4c8ad6afc17e6dab78f0fc
thesumonart <thesumonart.bd@gmail.com>
2025-01-28 20:03:21 +0600

    Engineering solutions section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       fontFamily: {
         Inter: ["Inter", "serif"],
+        SegoeUI: ["Segoe UI", "serif"],
       },
       maxWidth: {
         siteWidth: "1440px",
@@ -17,6 +18,8 @@ module.exports = {
       backgroundImage: {
         heroImage:
           "linear-gradient(180deg, #010314 40.24%, rgba(1, 3, 20, 0.00) 100%), radial-gradient(65.08% 85.05% at 50% 50%, rgba(0, 0, 0, 0.00) 37.41%, #63E 69.27%, #FFF 100%)",
+        engSolutions:
+          "linear-gradient(0deg, #010314 40.24%, rgba(1, 3, 20, 0.00) 100%), radial-gradient(65.08% 85.05% at 50% 50%, rgba(0, 0, 0, 0.00) 37.41%, #63E 69.27%, #FFF 100%)",
         textImage:
           "radial-gradient(100% 45% at 50% 50%, #FFF 30%, rgba(255, 255, 255, 0.35) 100%)",
       },
