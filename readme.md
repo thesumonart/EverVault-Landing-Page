683b98bc23f2a2db51f38d8aa91b97299d96b364
thesumonart <thesumonart.bd@gmail.com>
2025-01-28 14:30:33 +0600

    Hero section completed

---
+++ readme.md
@@ -0,0 +1,3 @@
+## To start this project with tailwind CLI use the following command.
+
+`npx tailwindcss -i ./src/input.css -o ./src/output.css --watch`
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*", "*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "serif"],
+      },
+      maxWidth: {
+        siteWidth: "1440px",
+        containerWidth: "1000px",
+      },
+      colors: {
+        richBlack: "#010314",
+        heroText: "#ECECFB",
+      },
+      backgroundImage: {
+        heroImage:
+          "linear-gradient(180deg, #010314 40.24%, rgba(1, 3, 20, 0.00) 100%), radial-gradient(65.08% 85.05% at 50% 50%, rgba(0, 0, 0, 0.00) 37.41%, #63E 69.27%, #FFF 100%)",
+        textImage:
+          "radial-gradient(100% 45% at 50% 50%, #FFF 30%, rgba(255, 255, 255, 0.35) 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
